ee42bbc8c380919955e45d18c3c4515d0549d832
Yuliya <sirena_travel@mail.ru>
2022-09-22 14:20:36 +0300

    Task 1_1 homework change

--- Homework/1_1.py
+++ Homework/1_1.py
@@ -1,1 +1,11 @@
-print('yes')
+# Напишите программу,
+# которая принимает на вход цифру, обозначающую день недели,
+# и проверяет, является ли этот день выходным.
+
+day_of_week_num = int(input('Enter the number of the day of the week: '))
+if day_of_week_num < 1 or day_of_week_num > 7:
+    print('Input error')
+elif day_of_week_num < 6:
+    print('Weekday')
+else:
+    print('Weekend')
