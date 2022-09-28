011d4dceac70d2862f0eeb9a2638fd1d6bfc889d
Yuliya <sirena_travel@mail.ru>
2022-09-28 21:07:55 +0300

    Task 2_4 homework

---
+++ Homework/2_4.py
@@ -0,0 +1,24 @@
+# 4. Напишите программу, которая принимает на вход 2 числа.
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях(не индексах).
+# Position one: 1
+# Position two: 3
+# Number of elements: 5
+# -> [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+# -> 15
+
+first_position = int(input('Enter a first position: ')) - 1
+second_position = int(input('Enter a second position: ')) - 1
+number = int(input('Enter a number of elements: '))
+
+list_numbers = []
+
+for i in range(-number, number + 1):
+    list_numbers.append(i)
+
+print(f'-> {list_numbers}')
+
+res = list_numbers[first_position] * list_numbers[second_position]
+
+print(res)
+
