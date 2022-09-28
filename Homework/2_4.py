c44b23f7d09a2d7e1a84ef133486c1288da83b96
Yuliya <sirena_travel@mail.ru>
2022-09-28 21:42:06 +0300

    Task 2_4 homework change

--- Homework/2_4.py
+++ Homework/2_4.py
@@ -11,14 +11,15 @@ first_position = int(input('Enter a first position: ')) - 1
 second_position = int(input('Enter a second position: ')) - 1
 number = int(input('Enter a number of elements: '))
 
-list_numbers = []
-
-for i in range(-number, number + 1):
-    list_numbers.append(i)
-
-print(f'-> {list_numbers}')
-
-res = list_numbers[first_position] * list_numbers[second_position]
-
-print(res)
+if first_position > (number * 2 + 1) or second_position > (number * 2 + 1):
+    print('Error input')
+elif first_position <= 0 or second_position <= 0:
+    print('Error input')
+else:
+    list_numbers = []
+    for i in range(-number, number + 1):
+        list_numbers.append(i)
+    print(f'-> {list_numbers}')
+    res = list_numbers[first_position] * list_numbers[second_position]
+    print(res)
 
