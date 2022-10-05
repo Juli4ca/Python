9cd4696cb87d08783d2491e4bd198117cf042c2f
Yuliya <sirena_travel@mail.ru>
2022-10-05 17:56:33 +0300

    Task 4_5 homework

--- Homework/4_4.py
+++ Homework/4_4.py
@@ -22,10 +22,37 @@ def polynomial(degree):
     with open('poly.txt', 'a', encoding='utf-8') as my_file:
         my_file.write(f'{str_polynomial}\n')
 
+def polynomial_1(degree):
+    list_polynomial = choices(range(10), k=degree)
+    for i in range(degree):
+        if list_polynomial[i] > 0:
+            polynom = list_polynomial[i]
+            break
+    sign = choice('+-')
+    str_polynomial = f'{polynom}*x^{degree} {sign} '
+
+    for i in range(degree - 1, 1, -1):
+        polynom = choice(list_polynomial)
+        if polynom > 0:
+            sign = choice('+-')
+            str_polynomial += f'{polynom}*x^{i} {sign} '
+    polynom = choice(list_polynomial)
+    str_polynomial += f'{polynom} = 0'
+    with open('poly_2.txt', 'a', encoding='utf-8') as my_file_1:
+        my_file_1.write(f'{str_polynomial}\n')
+
+
 
 for i in range(3):
     degree_1 = int(input("Задана натуральная степень k: "))
     if degree_1 > 2:
         polynomial(degree_1)
+    else:
+        print("Ошибка ввода")
+
+for i in range(3):
+    degree_2 = int(input("Задана натуральная степень k: "))
+    if degree_2 > 2:
+        polynomial_1(degree_2)
     else:
         print("Ошибка ввода")
