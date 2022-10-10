8e71b2514c686a35a9eb2b4edd61f8ac54cbf4a4
Yuliya <sirena_travel@mail.ru>
2022-10-10 22:12:59 +0300

    homework

--- Homework/4_5.py
+++ Homework/4_5.py
@@ -15,3 +15,23 @@ while True:
     str_line = line1[:-4] + '+ ' + line2
     with open('poly_all.txt', 'a', encoding='utf-8') as file_my:
         file_my.write(f'{str_line}\n')
+
+# from random import choice
+#
+#
+# def poly_sum(name_1: str, name_2: str):
+#     with open(name_1, "r", encoding="utf-8") as my_f_1, \
+#             open(name_2, "r", encoding="utf-8") as my_f_2:
+#         first = my_f_1.readlines()
+#         second = my_f_2.readlines()
+#
+#         if len(first) == len(second):
+#             with open("sum_poly.txt", "a", encoding="utf-8") as my_f_3:
+#                 for i, v in enumerate(first):
+#                     my_f_3.write(f"{v[:-5]} + {second[i]}")
+#         else:
+#             print("The contents of the files do not match!")
+#
+#
+# poly_sum(input("Enter the file name 'text_1.txt': "), input("Enter the file name 'text_2.txt': "))
+# poly_sum("poly.txt", "poly_2.txt")
